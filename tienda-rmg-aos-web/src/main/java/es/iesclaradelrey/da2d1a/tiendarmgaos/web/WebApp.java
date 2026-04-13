078d70797d382648cf6f6d6a21d6c8e3c41d62c8
Rodrigo M <rodrigomgbc@gmail.com>
2026-04-13 13:54:17 +0200

    avances hasta el punto 3.13, 3.14 a empezar

---
+++ tienda-rmg-aos-web/src/main/java/es/iesclaradelrey/da2d1a/tiendarmgaos/web/WebApp.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendarmgaos.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApp.class, args);
+    }
+
+}
---
+++ tienda-rmg-aos-web/src/test/java/es/iesclaradelrey/da2d1a/tiendarmgaos/web/TiendaRmgAosWebApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesclaradelrey.da2d1a.tiendarmgaos.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaRmgAosWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
