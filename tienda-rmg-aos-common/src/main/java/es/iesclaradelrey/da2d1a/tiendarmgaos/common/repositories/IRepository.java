d515768634859e0d81902dd224df6e30c80b4e78
Rodrigo Martín <rodrigomgbc@gmail.com>
2026-04-15 20:22:33 +0200

    Creación de la interfaz IRepository, primer "subapartado del 3.7", luego continuar con el segundo.

--- tienda-rmg-aos-common/src/main/java/es/iesclaradelrey/da2d1a/tiendarmgaos/common/repositories/IRepository.java
+++ tienda-rmg-aos-common/src/main/java/es/iesclaradelrey/da2d1a/tiendarmgaos/common/repositories/IRepository.java
@@ -1,7 +1,19 @@
 package es.iesclaradelrey.da2d1a.tiendarmgaos.common.repositories;
 
+import java.util.Collection;
+import java.util.Optional;
+
+/*
+   * T y id son tipos genericos. Se usan para que esta interfaz sirva para cualquier tipo de entidad y cualquier tipo
+   * de identificador. T representa la entidad con la que se va a trabajar e ID representa el tipo de dato de la clave
+   * primaria de esa entidad.
+ */
 public interface IRepository<T, ID> {
 
     T guardar(T entidad);
+    Optional<T> buscarPorId(ID id);
+    Collection<T> buscarTodos();
+    void eliminarPorId(ID id);
+    boolean existePorId(ID id);
 
 }
