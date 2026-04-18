fae7b0cd75449c859048c8b966f18c889a19b53e
Rodrigo Martín <rodrigomgbc@gmail.com>
2026-04-19 00:17:12 +0200

    Punto 3.3 del tercer pdf completado, mañana tengo que empezar y acabar el 3.4

--- tienda-rmg-aos-web/src/main/java/es/iesclaradelrey/da2d1a/tiendarmgaos/web/controllers/CategoryController.java
+++ tienda-rmg-aos-web/src/main/java/es/iesclaradelrey/da2d1a/tiendarmgaos/web/controllers/CategoryController.java
@@ -1,10 +1,14 @@
 package es.iesclaradelrey.da2d1a.tiendarmgaos.web.controllers;
 
+import es.iesclaradelrey.da2d1a.tiendarmgaos.common.entities.Category;
 import es.iesclaradelrey.da2d1a.tiendarmgaos.common.services.ICategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/categorias")
@@ -20,5 +24,15 @@ public class CategoryController {
     public String listar(Model model) {
         model.addAttribute("categorias", categoryService.buscarTodos());
         return "categorias/list";
+    }
+
+    @GetMapping("/{id}")
+    public String detalle(@PathVariable Long id, Model model) {
+        Optional<Category> categoria = categoryService.buscarPorId(id);
+        if (categoria.isEmpty()) {
+            return "redirect:/categorias";
+        }
+        model.addAttribute("categoria",categoria.get());
+        return "categorias/detail";
     }
 }
