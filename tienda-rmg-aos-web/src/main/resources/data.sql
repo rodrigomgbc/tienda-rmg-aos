8bfe6cf10939ea332fab00b9dac2a599cefb6bd6
Rodrigo M <rodrigomgbc@gmail.com>
2026-04-22 12:56:06 +0200

    apartado 3.4 del 4º pdf acabado, he creado data.sql y he eliminado la clase DataLoader. También he modificado el properties para que no de error el .sql

---
+++ tienda-rmg-aos-web/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO categories (nombre, descripcion, imagen, activo) VALUES ('Arábica', 'Cafés de variedad arábica...', null, true);
+INSERT INTO categories (nombre, descripcion, imagen, activo) VALUES ('Robusto', 'Cafés de variedad robusta...', null, true);
+INSERT INTO categories (nombre, descripcion, imagen, activo) VALUES ('Tueste Claro', 'Cafés con tueste claro...', null, true);
+INSERT INTO categories (nombre, descripcion, imagen, activo) VALUES ('Tueste Oscuro', 'Cafés con tueste oscuro...', null, true);
